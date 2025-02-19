f4620a49fbf6318b730d322669dd2b4daff88d54
Mark Wilkinson <markawil@yahoo.com>
2025-02-18 23:16:41 -0600

    Initial Commit

---
+++ CryptoApp/CryptoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAppApp.swift
+//  CryptoApp
+//
+//  Created by Mark Wilkinson on 2/18/25.
+//
+
+import SwiftUI
+
+@main
+struct CryptoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CryptoAppTests/CryptoAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAppTests.swift
+//  CryptoAppTests
+//
+//  Created by Mark Wilkinson on 2/18/25.
+//
+
+import Testing
+@testable import CryptoApp
+
+struct CryptoAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
